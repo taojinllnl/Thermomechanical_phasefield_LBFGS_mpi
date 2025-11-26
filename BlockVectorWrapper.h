81e844485851e99e6591ed8398d193dc51ffb93b
Zhao Li <zli385@uottawa.ca>
2025-11-26 11:56:01 -0500

    define a Vector Wrapper

---
+++ BlockVectorWrapper.h
@@ -0,0 +1,12 @@
+//
+//  BlockVectorWrapper.h
+//  main
+//
+//  Created by Ethan Lee on 2025-11-26.
+//
+
+#ifndef BlockVectorWrapper_h
+#define BlockVectorWrapper_h
+
+
+#endif /* BlockVectorWrapper_h */
