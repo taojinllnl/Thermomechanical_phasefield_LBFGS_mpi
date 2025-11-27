712ae463649f3bb0aa634051bc138d662f55476b
Zhao Li <zli385@uottawa.ca>
2025-11-27 17:11:27 -0500

    MPIInfo

---
+++ MPIInfo.cc
@@ -0,0 +1,8 @@
+//
+//  MPIInfo.cpp
+//  main
+//
+//  Created by Ethan Lee on 2025-11-27.
+//
+
+#include <stdio.h>
---
+++ MPIInfo.h
@@ -0,0 +1,12 @@
+//
+//  MPIInfo.h
+//  main
+//
+//  Created by Ethan Lee on 2025-11-27.
+//
+
+#ifndef MPIInfo_h
+#define MPIInfo_h
+
+
+#endif /* MPIInfo_h */
