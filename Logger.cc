98d64b137fbd1b7779879a4890410de9aeb292be
Zhao Li <zli385@uottawa.ca>
2025-11-28 11:45:28 -0500

    Logger

--- Logger.cc
+++ Logger.cc
@@ -2,7 +2,37 @@
 //  Logger.cpp
 //  main
 //
-//  Created by Ethan Lee on 2025-11-28.
 //
 
-#include "Logger.hpp"
+#include "Logger.h"
+
+
+
+Logger::Logger(const MPIInfo&       mpiInfo,
+               const std::string&   dir,
+               const std::string&   filename,
+               const unsigned int   selectedRank)
+: __mpiInfo(mpiInfo)
+, __isOn(mpiInfo.rank() == selectedRank)
+{
+    if(__isOn) {
+        std::string streamPath;
+        if(__mpiInfo.isMPI())
+            
+            streamPath = dir + "mpi_" + std::to_string(__mpiInfo.rank()) + "_" + std::to_string(__mpiInfo.nRanks()) + "_" + filename;
+        else
+            streamPath = dir + "serial_"+  filename;
+        
+        __ofstream = std::make_unique<std::ofstream>(streamPath);
+    }
+}
+
+
+
+Logger::~Logger()
+{
+    if (__ofstream) {
+        __ofstream->close();
+    }
+    
+}
--- Logger.h
+++ Logger.h
@@ -2,12 +2,76 @@
 //  Logger.hpp
 //  main
 //
-//  Created by Ethan Lee on 2025-11-28.
 //
 
 #ifndef Logger_hpp
 #define Logger_hpp
 
-#include <stdio.h>
+#include <fstream>
+#include <memory>
+
+#include "MPIInfo.h"
+
+
+class Logger
+{
+private:
+    const MPIInfo&   __mpiInfo;
+    
+    const bool       __isOn;
+    
+    mutable std::unique_ptr<std::ofstream>             __ofstream;
+public:
+    virtual ~Logger();
+    
+    
+    
+    Logger()                = delete;
+    Logger(const Logger&)   = delete;
+    Logger(Logger&&)        = delete;
+    
+    
+    Logger(const MPIInfo&       mpiInfo,
+           const std::string&   dir,
+           const std::string&   filename,
+           const unsigned int   selectedRank = 0);
+    
+    
+    // << operator
+    template <typename T>
+    friend Logger& operator<<(Logger& logger, const T& content);
+  
+    // << operator for manipulator, i.e. std::endl, std::flush
+    friend Logger& operator<<(Logger& logger,
+                              std::ostream& (*manip)(std::ostream&));
+    
+    
+};
+
+
+
+
+
+template <typename T>
+inline Logger&
+operator<<(Logger& logger, const T& content) 
+{
+    if(logger.__isOn)
+        *(logger.__ofstream) << content;
+        
+    return logger;
+}
+
+inline Logger&
+operator<<(Logger& logger,
+           std::ostream& (*manip)(std::ostream&))
+    {
+        if(logger.__isOn)
+            manip(*(logger.__ofstream));
+            
+        return logger;
+    }
+
+
 
 #endif /* Logger_hpp */
