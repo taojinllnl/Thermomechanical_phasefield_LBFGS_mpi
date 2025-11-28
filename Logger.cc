f8a5f51a3819ebf5d00b64a4b27a6f1fd4a9627d
Zhao Li <zli385@uottawa.ca>
2025-11-28 10:40:14 -0500

    logger

---
+++ Logger.cc
@@ -0,0 +1,8 @@
+//
+//  Logger.cpp
+//  main
+//
+//  Created by Ethan Lee on 2025-11-28.
+//
+
+#include "Logger.hpp"
---
+++ Logger.h
@@ -0,0 +1,13 @@
+//
+//  Logger.hpp
+//  main
+//
+//  Created by Ethan Lee on 2025-11-28.
+//
+
+#ifndef Logger_hpp
+#define Logger_hpp
+
+#include <stdio.h>
+
+#endif /* Logger_hpp */
