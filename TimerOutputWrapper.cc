b5b6313742bb9659f6bf172ec49879bb7ba71924
Zhao Li <zli385@uottawa.ca>
2025-11-29 09:33:23 -0500

    TimerOutputWrapper

--- TimerOutputWrapper.cc
+++ TimerOutputWrapper.cc
@@ -6,4 +6,116 @@
 
 #include "TimerOutputWrapper.h"
 
+using namespace dealii;
+using OutputFrequency   =  dealii::TimerOutput::OutputFrequency;
+using OutputType        =  dealii::TimerOutput::OutputType;
+using OutputData        =  dealii::TimerOutput::OutputData;
+
 TimerOutputWrapper
+::TimerOutputWrapper(std::ostream&  stream,
+                     const MPIInfo& mpiInfo,
+                     const OutputFrequency output_frequency,
+                     const OutputType output_type)
+: __mpiInfo(mpiInfo)
+{
+    if (__mpiInfo.isMPI()) {
+        __timerPtr = std::make_unique<TimerOutput>(*mpiInfo.mpiComm(),
+                                                   stream,
+                                                   output_frequency,
+                                                   output_type);
+    } else {
+        __timerPtr = std::make_unique<TimerOutput>(stream,
+                                                   output_frequency,
+                                                   output_type);
+    }
+    
+}
+
+TimerOutputWrapper
+::TimerOutputWrapper(ConditionalOStream &stream,
+                     const MPIInfo& mpiInfo,
+                     const OutputFrequency output_frequency,
+                     const OutputType output_type)
+: __mpiInfo(mpiInfo)
+{
+    if (__mpiInfo.isMPI()) {
+        __timerPtr = std::make_unique<TimerOutput>(*mpiInfo.mpiComm(),
+                                                   stream,
+                                                   output_frequency,
+                                                   output_type);
+    } else {
+        __timerPtr = std::make_unique<TimerOutput>(stream,
+                                                   output_frequency,
+                                                   output_type);
+    }
+}
+
+
+
+TimerOutput&
+TimerOutputWrapper
+::timer()
+{
+    return *__timerPtr;
+}
+
+
+
+
+void     
+TimerOutputWrapper
+::enter_subsection (const std::string& section_name)
+{
+    __timerPtr->enter_subsection(section_name);
+}
+
+
+void
+TimerOutputWrapper
+::leave_subsection (const std::string& section_name)
+{
+    __timerPtr->leave_subsection(section_name);
+}
+
+std::map< std::string, double >     
+TimerOutputWrapper
+::get_summary_data (const OutputData kind) const
+{
+    return __timerPtr->get_summary_data(kind);
+}
+
+void     
+TimerOutputWrapper
+::print_summary () const
+{
+    __timerPtr->print_summary();
+}
+
+void     
+TimerOutputWrapper
+::print_wall_time_statistics (const double print_quantile) const
+{
+    __timerPtr->print_wall_time_statistics(*__mpiInfo.mpiComm(),
+                                           print_quantile);
+}
+
+void     
+TimerOutputWrapper
+::disable_output ()
+{
+    __timerPtr->disable_output();
+}
+
+void     
+TimerOutputWrapper
+::enable_output ()
+{
+    __timerPtr->enable_output();
+}
+
+void     
+TimerOutputWrapper
+::reset ()
+{
+    __timerPtr->reset();
+}
--- TimerOutputWrapper.h
+++ TimerOutputWrapper.h
@@ -1,12 +1,61 @@
 //
-//  TimerOutputWraper.h
+//  TimerOutputWrapper.h
 //  main
 //
-//  Created by Ethan Lee on 2025-11-28.
 //
 
-#ifndef TimerOutputWraper_h
-#define TimerOutputWraper_h
+#ifndef TimerOutputWrapper_h
+#define TimerOutputWrapper_h
+
+#include <deal.II/base/timer.h>
+
+#include <ostream>
+#include <memory>
+
+#include "MPIInfo.h"
 
 
-#endif /* TimerOutputWraper_h */
+class TimerOutputWrapper
+{
+private:
+    
+    using OutputFrequency   =  dealii::TimerOutput::OutputFrequency;
+    using OutputType        =  dealii::TimerOutput::OutputType;
+    using OutputData        =  dealii::TimerOutput::OutputData;
+    
+    const MPIInfo&                          __mpiInfo;
+    
+    std::unique_ptr<dealii::TimerOutput>    __timerPtr;
+    
+    
+public:
+    virtual ~TimerOutputWrapper() = default;
+    
+    explicit TimerOutputWrapper(std::ostream&  stream,
+                                const MPIInfo& mpiInfo,
+                                const OutputFrequency output_frequency,
+                                const OutputType output_type);
+    
+    explicit TimerOutputWrapper(dealii::ConditionalOStream &stream,
+                                const MPIInfo& mpiInfo,
+                                const OutputFrequency output_frequency,
+                                const OutputType output_type);
+    
+    
+    
+    dealii::TimerOutput& timer();
+    
+    
+    
+    void enter_subsection (const std::string &section_name);
+    void leave_subsection (const std::string &section_name="");
+    std::map<std::string, double> get_summary_data (const OutputData kind) const;
+    void print_summary () const;
+    void print_wall_time_statistics (const double print_quantile=0.) const;
+    void disable_output ();
+    void enable_output ();
+    void reset ();
+    
+};
+
+#endif /* TimerOutputWrapper_h */
