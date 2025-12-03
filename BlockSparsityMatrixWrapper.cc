be636c8f04739eee624a2451cb70e2d5085bacb6
Zhao Li <zli385@uottawa.ca>
2025-12-03 14:48:34 -0500

    BlockSparsityMatrixWrapper

---
+++ BlockSparsityMatrixWrapper.cc
@@ -0,0 +1,8 @@
+//
+//  BlockSparsityMatrixWrapper.cpp
+//  main
+//
+//  Created by Ethan Lee on 2025-12-03.
+//
+
+#include "BlockSparsityMatrixWrapper.hpp"
---
+++ BlockSparsityMatrixWrapper.h
@@ -0,0 +1,13 @@
+//
+//  BlockSparsityMatrixWrapper.hpp
+//  main
+//
+//  Created by Ethan Lee on 2025-12-03.
+//
+
+#ifndef BlockSparsityMatrixWrapper_hpp
+#define BlockSparsityMatrixWrapper_hpp
+
+#include <stdio.h>
+
+#endif /* BlockSparsityMatrixWrapper_hpp */
