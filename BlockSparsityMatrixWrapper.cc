845d6a3d2dcf2c5172618d53599e67a92d92793b
Zhao Li <zli385@uottawa.ca>
2025-12-03 14:48:47 -0500

    BlockSparsityMatrixWrapper

--- BlockSparsityMatrixWrapper.cc
+++ BlockSparsityMatrixWrapper.cc
@@ -2,7 +2,48 @@
 //  BlockSparsityMatrixWrapper.cpp
 //  main
 //
-//  Created by Ethan Lee on 2025-12-03.
 //
 
-#include "BlockSparsityMatrixWrapper.hpp"
+#include "BlockSparsityMatrixWrapper.h"
+using namespace la;
+
+template <typename TraitsType>
+using BSMatrix = BlockSparsityMatrixWrapper<TraitsType>;
+
+
+template <typename TraitsType>
+typename BSMatrix<TraitsType>::Coupling
+BlockSparsityMatrixWrapper<TraitsType>
+::__couplingInit(const BlockDesc& blockDesc,
+                 const BSMatrix<TraitsType>::CouplingFunc& func)
+{
+    const unsigned int nComponents = blockDesc.nComponents();
+    
+    Coupling coupling(nComponents, nComponents);
+    
+    for (unsigned int ii = 0; ii < nComponents; ++ii)
+        for (unsigned int jj = 0; jj < nComponents; ++jj)
+            coupling[ii][jj] = func(ii, jj);
+            
+    return coupling;
+}
+
+
+
+template <typename TraitsType>
+BlockSparsityMatrixWrapper<TraitsType>
+::BlockSparsityMatrixWrapper(const MPIInfo& mpiInfo,
+                             const BlockDesc& blockDesc,
+                             const BSMatrix<TraitsType>::CouplingFunc& func)
+: __mpiInfo(mpiInfo)
+, __blockDesc(blockDesc)
+, __coupling(BSMatrix<TraitsType>::__couplingInit(blockDesc, func))
+{}
+
+
+
+
+
+template class la::BlockSparsityMatrixWrapper<la::Traits<TagSerial>>;
+template class la::BlockSparsityMatrixWrapper<la::Traits<TagPETSc>>;
+template class la::BlockSparsityMatrixWrapper<la::Traits<TagTrilinos>>;
--- BlockSparsityMatrixWrapper.h
+++ BlockSparsityMatrixWrapper.h
@@ -2,12 +2,110 @@
 //  BlockSparsityMatrixWrapper.hpp
 //  main
 //
-//  Created by Ethan Lee on 2025-12-03.
 //
 
 #ifndef BlockSparsityMatrixWrapper_hpp
 #define BlockSparsityMatrixWrapper_hpp
 
-#include <stdio.h>
+#include <functional>
+
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/lac/block_sparsity_pattern.h>
+#include <deal.II/lac/affine_constraints.h>
+#include <deal.II/lac/sparsity_tools.h>
+
+
+
+#include "MPIInfo.h"
+#include "Traits.h"
+
+#include "BlockDesc.h"
+
+namespace la
+{
+
+template <typename TraitsType>
+class BlockSparsityMatrixWrapper
+: public TraitsType::Matrix
+{
+private:
+    using MatType       = typename TraitsType::Matrix;
+    using CouplingItem  = dealii::DoFTools::Coupling;
+    using Coupling      = dealii::Table<2, CouplingItem>;
+    using CouplingFunc  = std::function<CouplingItem(const unsigned int,
+                                                     const unsigned int)>;
+    
+    
+    
+    
+    static Coupling __couplingInit(const BlockDesc& blockDesc,
+                                   const CouplingFunc& func);
+    
+    
+    const MPIInfo&              __mpiInfo;
+    const BlockDesc&            __blockDesc;
+    const Coupling              __coupling;
+    
+    dealii::BlockSparsityPattern        __sparsity_pattern;
+public:
+    virtual ~BlockSparsityMatrixWrapper() = default;
+    
+    BlockSparsityMatrixWrapper() = delete;
+    BlockSparsityMatrixWrapper(const MPIInfo& mpiInfo,
+                               const BlockDesc& blockDesc,
+                               const CouplingFunc& func);
+    
+    
+    template <int dim, int spacedim=dim>
+    void init(dealii::DoFHandler<dim, spacedim>& dof_handler,
+              const dealii::AffineConstraints<double>&  constraints = {},
+              const bool keep_constrained_dofs = true,
+              const dealii::types::subdomain_id subdomain_id = dealii::numbers::invalid_subdomain_id );
+    
+};
+
+
+
+template <typename TraitsType>
+template <int dim, int spacedim>
+void
+BlockSparsityMatrixWrapper<TraitsType>
+::init(dealii::DoFHandler<dim, spacedim>&       dof_handler,
+       const dealii::AffineConstraints<double>& constraints,
+       const bool                               keep_constrained_dofs,
+       const dealii::types::subdomain_id        subdomain_id)
+{
+    using namespace dealii;
+    
+    TraitsType::Matrix::clear();
+    
+    const std::vector<IndexSet>& ownedPartition = *__blockDesc.ownedPartitionint();
+    const std::vector<IndexSet>& relevPartition = *__blockDesc.relevantPartitionint();
+    
+    BlockDynamicSparsityPattern dsp(*__blockDesc.relevantPartitionint());
+    DoFTools::make_sparsity_pattern(dof_handler, 
+                                    __coupling,
+                                    dsp, constraints,
+                                    keep_constrained_dofs,
+                                    subdomain_id);
+    __sparsity_pattern.copy_from(dsp);
+    
+    
+    SparsityTools::distribute_sparsity_pattern(dsp,
+                                               ownedPartition,
+                                               __mpiInfo.mpiComm(),
+                                               relevPartition);
+    
+
+    TraitsType::Matrix::reinit(ownedPartition,
+                               dsp,
+                               __mpiInfo.mpiComm());
+}
+
+
+
+
+}
 
 #endif /* BlockSparsityMatrixWrapper_hpp */
