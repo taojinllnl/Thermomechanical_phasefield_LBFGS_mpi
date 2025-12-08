d55647ad6ef33d5e6a0fa02a971eaba869cf9c44
Zhao Li <zli385@uottawa.ca>
2025-12-08 14:46:46 -0500

    LBFGSSolver for serial case

--- LBFGSSolver.cc
+++ LBFGSSolver.cc
@@ -6,95 +6,136 @@
 
 #include "LBFGSSolver.h"
 
-using namespace la;
+using namespace PhaseField_monolithic;
+
+
+template <typename LATraits>
+LBFGSSolver<LATraits>
+::LBFGSSolver(const SolverType&   type,
+              const double        cg_u_tol,
+              const double        cg_d_tol,
+              const double        cg_T_tol,
+              const BlockDesc&    blockDesc)
+: __type(type)
+, __cg_u_tol(cg_u_tol)
+, __cg_d_tol(cg_d_tol)
+, __cg_T_tol(cg_T_tol)
+, __u_group_ID(blockDesc.ithGroupID("displacement"))
+, __d_group_ID(blockDesc.ithGroupID("phase-field"))
+, __T_group_ID(blockDesc.ithGroupID("temperature"))
+{}
+
+
+
 
 template <typename LATraits>
 void 
-LBFGSSolver<LATraits>::solve()
+LBFGSSolver<LATraits>::solve(BVector & LBFGS_r_vector,
+                             BVector & LBFGS_q_vector,
+                             BSMatrix& tangent_matrix)
 {
-//        if (m_parameters.m_type_linear_solver == "Direct")
-//          {
-//        /*
-//        SparseDirectUMFPACK A_direct;
-//        A_direct.initialize(m_tangent_matrix);
-//        A_direct.vmult(LBFGS_r_vector,
-//                   LBFGS_q_vector);
-//                   */
-//
-//        // Performing LU decomposition on each block is much faster than
-//        // performing LU decomposition on the whole system
-//        SparseDirectUMFPACK A_direct_u;
-//        A_direct_u.initialize(m_tangent_matrix.block(m_u_dof, m_u_dof));
-//        A_direct_u.vmult(LBFGS_r_vector.block(m_u_dof),
-//                     LBFGS_q_vector.block(m_u_dof));
-//
-//        SparseDirectUMFPACK A_direct_d;
-//        A_direct_d.initialize(m_tangent_matrix.block(m_d_dof, m_d_dof));
-//        A_direct_d.vmult(LBFGS_r_vector.block(m_d_dof),
-//                     LBFGS_q_vector.block(m_d_dof));
-//
-//        SparseDirectUMFPACK A_direct_t;
-//        A_direct_t.initialize(m_tangent_matrix.block(m_t_dof, m_t_dof));
-//        A_direct_t.vmult(LBFGS_r_vector.block(m_t_dof),
-//                     LBFGS_q_vector.block(m_t_dof));
-//
-//          }
-//        else if (m_parameters.m_type_linear_solver == "CG")
-//          {
-//    /*
-//        SolverControl            solver_control(1e6, 1e-9);
-//        SolverCG<BlockVector<double>> cg(solver_control);
-//
-//        PreconditionJacobi<BlockSparseMatrix<double>> preconditioner;
-//        preconditioner.initialize(m_tangent_matrix, 1.0);
-//
-//        cg.solve(m_tangent_matrix,
-//             LBFGS_r_vector,
-//             LBFGS_q_vector,
-//             preconditioner);
-//    */
-//        SolverControl            solver_control_uu(1e6, m_parameters.m_cg_u_tol);
-//        SolverCG<Vector<double>> cg_uu(solver_control_uu);
-//
-//        PreconditionJacobi<SparseMatrix<double>> preconditioner_uu;
-//        preconditioner_uu.initialize(m_tangent_matrix.block(m_u_dof, m_u_dof), 1.0);
-//        cg_uu.solve(m_tangent_matrix.block(m_u_dof, m_u_dof),
-//                    LBFGS_r_vector.block(m_u_dof),
-//                    LBFGS_q_vector.block(m_u_dof),
-//                    preconditioner_uu);
-//
-//        SolverControl            solver_control_dd(1e6, m_parameters.m_cg_d_tol);
-//        SolverCG<Vector<double>> cg_dd(solver_control_dd);
-//
-//        PreconditionJacobi<SparseMatrix<double>> preconditioner_dd;
-//        preconditioner_dd.initialize(m_tangent_matrix.block(m_d_dof, m_d_dof), 1.0);
-//        cg_dd.solve(m_tangent_matrix.block(m_d_dof, m_d_dof),
-//                    LBFGS_r_vector.block(m_d_dof),
-//                    LBFGS_q_vector.block(m_d_dof),
-//                    preconditioner_dd);
-//
-//        SolverControl            solver_control_tt(1e6, m_parameters.m_cg_t_tol);
-//        SolverCG<Vector<double>> cg_tt(solver_control_tt);
-//
-//        PreconditionJacobi<SparseMatrix<double>> preconditioner_tt;
-//        preconditioner_tt.initialize(m_tangent_matrix.block(m_t_dof, m_t_dof), 1.0);
-//        cg_tt.solve(m_tangent_matrix.block(m_t_dof, m_t_dof),
-//                    LBFGS_r_vector.block(m_t_dof),
-//                    LBFGS_q_vector.block(m_t_dof),
-//                    preconditioner_tt);
-//          }
-//        else
-//          {
-//        AssertThrow(false,
-//                    ExcMessage("Selected linear solver not implemented!"));
-//          }
-//
+    if (__type == SolverType::Direct) {
+        __directSolve(LBFGS_r_vector, LBFGS_q_vector, tangent_matrix);
+    } else {
+        __cgSolve(LBFGS_r_vector, LBFGS_q_vector, tangent_matrix);
+    }
+}
 
+
+
+template <typename LATraits>
+void
+LBFGSSolver<LATraits>::__directSolve(BVector & LBFGS_r_vector,
+                                     BVector & LBFGS_q_vector,
+                                     BSMatrix& tangent_matrix)
+{
+    using namespace dealii;
+    if constexpr (std::is_same_v<typename LATraits::TMTag, ::la::TagSerial>) {
+        /*
+         SparseDirectUMFPACK A_direct;
+         A_direct.initialize(m_tangent_matrix);
+         A_direct.vmult(LBFGS_r_vector,
+         LBFGS_q_vector);
+         */
+        
+        // Performing LU decomposition on each block is much faster than
+        // performing LU decomposition on the whole system
+        SparseDirectUMFPACK A_direct_u;
+        A_direct_u.initialize(tangent_matrix.block(__u_group_ID, __u_group_ID));
+        A_direct_u.vmult(LBFGS_r_vector.block(__u_group_ID),
+                         LBFGS_q_vector.block(__u_group_ID));
+        
+        SparseDirectUMFPACK A_direct_d;
+        A_direct_d.initialize(tangent_matrix.block(__d_group_ID, __d_group_ID));
+        A_direct_d.vmult(LBFGS_r_vector.block(__d_group_ID),
+                         LBFGS_q_vector.block(__d_group_ID));
+        
+        SparseDirectUMFPACK A_direct_t;
+        A_direct_t.initialize(tangent_matrix.block(__T_group_ID, __T_group_ID));
+        A_direct_t.vmult(LBFGS_r_vector.block(__T_group_ID),
+                         LBFGS_q_vector.block(__T_group_ID));
+    } else {
+        
+    }
+}
+
+template <typename LATraits>
+void
+LBFGSSolver<LATraits>::__cgSolve(BVector & LBFGS_r_vector,
+                                 BVector & LBFGS_q_vector,
+                                 BSMatrix& tangent_matrix)
+{
+    using namespace dealii;
+    if constexpr (std::is_same_v<typename LATraits::TMTag, ::la::TagSerial>) {
+        /*
+         SolverControl            solver_control(1e6, 1e-9);
+         SolverCG<BlockVector<double>> cg(solver_control);
+         
+         PreconditionJacobi<BlockSparseMatrix<double>> preconditioner;
+         preconditioner.initialize(m_tangent_matrix, 1.0);
+         
+         cg.solve(m_tangent_matrix,
+         LBFGS_r_vector,
+         LBFGS_q_vector,
+         preconditioner);
+         */
+        SolverControl            solver_control_uu(1e6, __cg_u_tol);
+        SolverCG<Vector<double>> cg_uu(solver_control_uu);
+        
+        PreconditionJacobi<SparseMatrix<double>> preconditioner_uu;
+        preconditioner_uu.initialize(tangent_matrix.block(__u_group_ID, __u_group_ID), 1.0);
+        cg_uu.solve(tangent_matrix.block(__u_group_ID, __u_group_ID),
+                    LBFGS_r_vector.block(__u_group_ID),
+                    LBFGS_q_vector.block(__u_group_ID),
+                    preconditioner_uu);
+        
+        SolverControl            solver_control_dd(1e6, __cg_d_tol);
+        SolverCG<Vector<double>> cg_dd(solver_control_dd);
+        
+        PreconditionJacobi<SparseMatrix<double>> preconditioner_dd;
+        preconditioner_dd.initialize(tangent_matrix.block(__d_group_ID, __d_group_ID), 1.0);
+        cg_dd.solve(tangent_matrix.block(__d_group_ID, __d_group_ID),
+                    LBFGS_r_vector.block(__d_group_ID),
+                    LBFGS_q_vector.block(__d_group_ID),
+                    preconditioner_dd);
+        
+        SolverControl            solver_control_tt(1e6, __cg_T_tol);
+        SolverCG<Vector<double>> cg_tt(solver_control_tt);
+        
+        PreconditionJacobi<SparseMatrix<double>> preconditioner_tt;
+        preconditioner_tt.initialize(tangent_matrix.block(__T_group_ID, __T_group_ID), 1.0);
+        cg_tt.solve(tangent_matrix.block(__T_group_ID, __T_group_ID),
+                    LBFGS_r_vector.block(__T_group_ID),
+                    LBFGS_q_vector.block(__T_group_ID),
+                    preconditioner_tt);
+    } else {
+        
+    }
 }
 
 
 
 
-template class la::LBFGSSolver<la::Traits<la::TagSerial>>;
-template class la::LBFGSSolver<la::Traits<la::TagPETSc>>;
-template class la::LBFGSSolver<la::Traits<la::TagTrilinos>>;
+template class PhaseField_monolithic::LBFGSSolver<la::Traits<la::TagSerial>>;
+template class PhaseField_monolithic::LBFGSSolver<la::Traits<la::TagPETSc>>;
+template class PhaseField_monolithic::LBFGSSolver<la::Traits<la::TagTrilinos>>;
--- LBFGSSolver.h
+++ LBFGSSolver.h
@@ -7,23 +7,62 @@
 #ifndef LBFGSSolver_hpp
 #define LBFGSSolver_hpp
 
+#include <deal.II/lac/sparse_direct.h>
+
 #include "Traits.h"
 
 #include "BlockVectorWrapper.h"
 #include "BlockSparseMatrixWrapper.h"
 
-namespace la {
+#include "BlockDesc.h"
+
+
+namespace PhaseField_monolithic {
+
+enum class SolverType
+{
+    Direct, CG
+};
 
 template <typename LATraits>
 class LBFGSSolver
 {
 public:
-    using BSMatrix = BlockSparseMatrixWrapper<LATraits>;
-    using BVector  = BlockVectorWrapper<LATraits>;
+    using BSMatrix  = ::la::BlockSparseMatrixWrapper<LATraits>;
+    using BVector   = ::la::BlockVectorWrapper<LATraits>;
+    
+private:
+    const SolverType        __type;
+    const double            __cg_u_tol;
+    const double            __cg_d_tol;
+    const double            __cg_T_tol;
+    
+    const unsigned int      __u_group_ID;
+    const unsigned int      __d_group_ID;
+    const unsigned int      __T_group_ID;
+    
+    
+    void __directSolve(BVector & LBFGS_r_vector,
+                       BVector & LBFGS_q_vector,
+                       BSMatrix& tangentMatrix);
+    void __cgSolve(BVector & LBFGS_r_vector,
+                   BVector & LBFGS_q_vector,
+                   BSMatrix& tangentMatrix);
+    
+public:
     
     virtual ~LBFGSSolver() = default;
     
-    void solve();
+    
+    LBFGSSolver(const SolverType&   type,
+                const double        cg_u_tol,
+                const double        cg_d_tol,
+                const double        cg_T_tol,
+                const BlockDesc&    blockDesc);
+    
+    void solve(BVector & LBFGS_r_vector,
+               BVector & LBFGS_q_vector,
+               BSMatrix& tangentMatrix);
     
 };
 
