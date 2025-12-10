ca7776ce36336a2525e86b1fe1fd7e1298446326
Zhao Li <zli385@uottawa.ca>
2025-12-10 13:39:00 -0500

    InverseMatrix to find B0 with CG solver in MPI mode

--- InverseMatrix.h
+++ InverseMatrix.h
@@ -2,11 +2,70 @@
 //  InverseMatrix.h
 //  main
 //
-//  Created by Ethan Lee on 2025-12-09.
 //
 
 #ifndef InverseMatrix_h
 #define InverseMatrix_h
 
+
+#include <deal.II/lac/solver_cg.h>
+
+
+
+template <typename MatrixType, typename PreconditionerType>
+class InverseMatrix 
+: public dealii::Subscriptor
+{
+private:
+    const dealii::SmartPointer<const MatrixType> matrix;
+    const PreconditionerType&            preconditioner;
+    
+public:
+    virtual ~InverseMatrix() = default;
+    
+    InverseMatrix(const MatrixType&         m,
+                  const PreconditionerType& preconditioner);
+    
+    
+    template <typename VectorType>
+    void vmult(dealii::SolverControl&    solver_control,
+               VectorType&               dst,
+               const VectorType&         src) const;
+    
+
+};
+
+
+template <typename MatrixType, typename PreconditionerType>
+InverseMatrix<MatrixType, PreconditionerType>
+::InverseMatrix(const MatrixType         &m,
+                const PreconditionerType &preconditioner)
+: matrix(&m)
+, preconditioner(preconditioner)
+{}
+
+
+
+template <typename MatrixType, typename PreconditionerType>
+template <typename VectorType>
+void InverseMatrix<MatrixType, PreconditionerType>
+::vmult(dealii::SolverControl&      solver_control,
+        VectorType&                 dst,
+        const VectorType&           src) const
+{
+    using namespace dealii;
+    SolverCG<VectorType> cg(solver_control);
+    
+    dst = 0.;
+    
+    try
+    {
+        cg.solve(*matrix, dst, src, preconditioner);
+    }
+    catch (std::exception &e)
+    {
+        Assert(false, ExcMessage(e.what()));
+    }
+}
 
 #endif /* InverseMatrix_h */
