dfe8d907e0141598f5bf08bffe147dc5374e32c5
Zhao Li <zli385@uottawa.ca>
2025-12-19 09:32:29 -0500

    skip ghost cells in L2 projection during solution transferring

--- main.cc
+++ main.cc
@@ -6107,7 +6107,16 @@ bool PhaseFieldMonolithicSolve<LATraits, Tria>::local_refine_and_solution_transf
                                  [&] (const typename DoFHandler<dim>::active_cell_iterator & cell,
                                       const unsigned int q) -> double
                                  {
-                return m_quadrature_point_history.get_data(cell)[q]->get_history_max_positive_strain_energy();
+                
+                if constexpr (is_mpi)
+                {
+                    if (cell->is_locally_owned())
+                        return m_quadrature_point_history.get_data(cell)[q]->get_history_max_positive_strain_energy();
+                    else
+                        return 0.0;
+                } else {   
+                    return m_quadrature_point_history.get_data(cell)[q]->get_history_max_positive_strain_energy();
+                }
             },
                                  old_history_variable_field_L2);
             
