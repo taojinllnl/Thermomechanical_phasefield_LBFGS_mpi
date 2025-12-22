0705c48827b524485b76dbd153eb98d2840bd75f
Zhao Li <zli385@uottawa.ca>
2025-12-22 17:06:16 -0500

    VersionAdapter

---
+++ VersionAdapter.cc
@@ -0,0 +1,8 @@
+//
+//  VersionAdapter.cpp
+//  main
+//
+//  Created by Ethan Lee on 2025-12-22.
+//
+
+#include <stdio.h>
---
+++ VersionAdapter.h
@@ -0,0 +1,12 @@
+//
+//  VersionAdapter.h
+//  main
+//
+//  Created by Ethan Lee on 2025-12-22.
+//
+
+#ifndef VersionAdapter_h
+#define VersionAdapter_h
+
+
+#endif /* VersionAdapter_h */
