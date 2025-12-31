93b610641292e6856a48972d8f31b37d22086153
Zhao Li <zli385@uottawa.ca>
2025-12-31 08:46:19 -0500

    define the structure of WorkloadBalancer

--- WorkloadBalancer.h
+++ WorkloadBalancer.h
@@ -7,14 +7,26 @@
 #ifndef WorkloadBalancer_h
 #define WorkloadBalancer_h
 
+#include <vector>
+#include <string>
 
+#include <deal.II/base/utilities.h>
+#include <deal.II/dofs/dof_handler.h>
 
+#include "MPIInfo.h"
+
+template <typename Tria>
 class WorkloadBalancer
 {
+private:
+    const MPIInfo& __mpiInfo;
+    
 public:
     virtual ~WorkloadBalancer() = default;
     
+    WorkloadBalancer(const MPIInfo& mpiInfo);
     
+    void balance();
 };
 
 
