dafee9d4b328915fcbbaea09ab0be2fc2c5678de
Zhao Li <zli385@uottawa.ca>
2026-02-17 13:12:50 -0500

    comment in BlockSparseMatrixWrapper

--- include/BlockSparseMatrixWrapper.h
+++ include/BlockSparseMatrixWrapper.h
@@ -25,6 +25,15 @@
 namespace la
 {
 
+
+/**
+ * This class is a light-weight wrapper for a specific type of BlockSparseMatrix defined in `TraitsType` for both serial and MPI modes.
+ *
+ *  This class maintains `::dealii::DoFTools::Coupling` for initialize during constrution.
+ *  Once the requied couplings are set, call `initalize()` for newly refined or genenrated meshes.
+ *
+ */
+
 template <typename TraitsType>
 class BlockSparseMatrixWrapper
 : public TraitsType::Matrix
